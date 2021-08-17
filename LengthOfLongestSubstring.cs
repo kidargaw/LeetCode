4fe25ec1a1b77aac3534a2c9fbf07822b17cd777
Kidanemariam Argaw <kidargaw@gmail.com>
2021-08-17 10:27:50 -0400

    Create LengthOfLongestSubstring.cs
    Given a string s, find the length of the longest substring without repeating characters.
    
    Example 1:
    
    Input: s = "abcabcbb"
    Output: 3
    Explanation: The answer is "abc", with the length of 3.
    Example 2:
    
    Input: s = "bbbbb"
    Output: 1
    Explanation: The answer is "b", with the length of 1.
    Example 3:
    
    Input: s = "pwwkew"
    Output: 3
    Explanation: The answer is "wke", with the length of 3.
    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
    Example 4:
    
    Input: s = ""
    Output: 0
     
    
    Constraints:
    
    0 <= s.length <= 5 * 104
    s consists of English letters, digits, symbols and spaces.

---
+++ LengthOfLongestSubstring.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+    if (s == "")
+                return 0;
+            int result = 0;
+
+            HashSet<char> hs = new HashSet<char>();
+            int index = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s.ElementAt(i);
+                if(!hs.Add(c))                
+                {
+                    if (hs.Count > result)
+                        result = hs.Count;
+                    
+                    hs.Clear();
+                    i = s.IndexOf(c, index);
+                    index = i + 1;
+                }       
+            }
+            if (hs.Count > result)
+                result = hs.Count;
+            return result;
+    }
+}
